f126a2ff9b31eef11406357a482c528fcbd9e3ff
jmf0991 <jmf0991@gmail.com>
2019-07-19 12:07:49 +0200

    added entity framwork, and 2 models

---
+++ HelloWorldV1/Models/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldV1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string ActorName { get; set; }
+        public string ShortBio { get; set; }
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public virtual Movie movie { get; set; }
+        public virtual Actor actor { get; set; }
+    }
+}
