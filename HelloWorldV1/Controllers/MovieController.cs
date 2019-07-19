ac9a00af797222d8db8c333a8c39758e3215a75a
jmf0991 <jmf0991@gmail.com>
2019-07-19 13:31:36 +0200

    Edit and Delete methods implemented, 'Add' link implemnted

--- HelloWorldV1/Controllers/MovieController.cs
+++ HelloWorldV1/Controllers/MovieController.cs
@@ -12,20 +12,22 @@ namespace HelloWorldV1.Controllers
         // GET: Movie
         public ActionResult Index()
         {
-            using (var db = new Models.MovieDBContext())
+            using (var db = new MovieDBContext())
             {
                 var movies = db.Movies.ToList();
                 return View(movies);
             }
         }
+
         [HttpGet]
         public ActionResult Add()
         {
-            Models.Movie movie = new Models.Movie();
+            Models.Movie movie = new Movie();
             return View(movie);
         }
+
         [HttpPost]
-        public ActionResult Add(Models.Movie movieFromView)
+        public ActionResult Add(Movie movieFromView)
         {
             if (ModelState.IsValid)
             {
@@ -39,6 +41,68 @@ namespace HelloWorldV1.Controllers
             }
 
             return View(movieFromView);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id = 0)
+        {
+            using (var db = new MovieDBContext())
+            {
+                Movie movie = db.Movies.Find(id);
+                // Find the movie from db 
+                if (movie == null) // There is no movie with the passed id 
+                {
+                    return HttpNotFound();
+                }
+                return View(movie);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie movieToUpdate)
+        {
+            if (ModelState.IsValid)
+            {
+                //Update the data in db and show the list of movies 
+                using (var db = new MovieDBContext())
+                {
+                    db.Entry(movieToUpdate).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            return View(movieToUpdate);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id = 0)
+        {
+            using (var db = new MovieDBContext())
+            {
+                Movie movie = db.Movies.Find(id);
+                // Find the movie from db 
+                if (movie == null) // There is no movie with the passed id 
+                {
+                    return HttpNotFound();
+                }
+                return View(movie);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Movie movieToDelete)
+        {
+            if (ModelState.IsValid)
+            {
+                //Update the data in db and show the list of movies 
+                using (var db = new MovieDBContext())
+                {
+                    db.Entry(movieToDelete).State = System.Data.Entity.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            return View(movieToDelete);
         }
     }
 }
