decae7f5c4fc1a2c6043e0f50e247a5e46c17019
jmf0991 <jmf0991@gmail.com>
2019-07-19 11:02:18 +0200

    project set up

---
+++ HelloWorldV1/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldV1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            Models.Movies movies = new Models.Movies();
+            movies.SeedMovies();
+            List<Models.Movie> moviesList = movies.GetMoviesList();
+            return View(moviesList);
+        }
+        public ActionResult MovieOfTheMonth()
+        {
+            return View();
+        }
+    }
+}
